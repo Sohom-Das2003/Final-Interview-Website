e391a2cdd32ae78d7fb31e4f4e70f0ff611d053a
Sohom-Das2003 <sohom.11a.60.17112003@gmail.com>
2025-03-03 14:05:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FigmaDesign from './FigmaDesign';
+import SignUp from './SignupPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FigmaDesign />
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
+
